e4ad15e539b990fd189e9f5d1bcec55bf86be067
Damith Thiwanka <damith.chandrasiri@interblocks.com>
2023-02-18 14:28:05 +0530

    edit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:apiswaggerdemo1/main.dart';
+import 'package:apiswaggerdemo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
